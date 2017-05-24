96586ddcbd953eeac16b00813d4c5036d68dc577
Stuart Schafer <stuartschafer@Stuarts-MacBook-Pro.local>
2017-05-24 17:44:15 -0400

    :bullettrain_side:FINISHED and added extra features such as timeframe

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var express = require("express");
 var routes = require("./controllers/burrito_controller.js");
 
 var app = express();
+
+// Must have this for heroku to work correct
 var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + "/public"));
