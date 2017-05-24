edaabb10d6ef118ecaa8db79165ae5508d12ed8b
Stuart Schafer <stuartschafer@Stuarts-MacBook-Pro.local>
2017-05-23 22:56:50 -0400

    :city_sunrise:First step in heroku

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,16 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
   database: "burrito_bucket"
 });
+}
 
 // Make connection
 connection.connect(function(err) {
